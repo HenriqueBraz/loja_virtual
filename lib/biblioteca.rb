9f8d8a2faf52d8daee5563406fd05cf85bb3702f
HenriqueBraz <hbraz9@gmail.com>
2020-12-06 23:48:50 -0300

    primeiro commit

---
+++ lib/biblioteca.rb
@@ -0,0 +1,17 @@
+class Biblioteca
+	attr_reader :livros
+	def initialize
+		@livros = {}  # Inicializa um hash
+	end
+
+
+	def adiciona(livro)
+		@livros[livro.categoria]  ||= []
+                @livros[livro.categoria] << livro
+	end
+
+	def livros
+		@livros.values.flatten
+	end	
+end	
+
