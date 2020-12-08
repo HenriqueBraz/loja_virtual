bd0afa9a6a483eb4b1ef4d086ac1ca4e82b14331
HenriqueBraz <hbraz9@gmail.com>
2020-12-07 23:36:37 -0300

    melhor-do-que-antes

--- lib/livro.rb
+++ lib/livro.rb
@@ -1,7 +1,6 @@
 # coding: utf-8
 class Livro
-        attr_accessor :preco
-	attr_reader :categoria
+	attr_reader :isbn
 	def initialize(autor, isbn="1", numero_de_paginas, valor, categoria)
 		@autor = autor
 		@isbn = isbn
@@ -9,9 +8,17 @@ class Livro
 		@categoria = categoria
 	end
 
+	def eql?(outro_livro)
+		@isbn == outro_livro.isbn
+	end	
+
 	def to_s
 	"Autor: #{@autor}, Isbn: #{@isbn}, Páginas: #{@numero_de_paginas}"
 	end
+
+	def hash
+	  @isbn.hash
+        end	  
 end
 
 
